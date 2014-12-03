d3eb6506bb0302f2477e0dc1152c9d7b0d0841f7
I am not me <impeccabletiming@gmail.com>
2014-12-02 19:05:21 -0500

    Initial commit

---
+++ i18n/selflink.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+msg = {
+    'en': {
+        'selflink-remove': u'Bot: Removing selflinks',
+    },
+    # Author: Base
+    'qqq': {
+        'selflink-remove': u'Default edit summary for when the bot removes selflinks from an article. Selflinks are links to the same page they are written in.',
+    },
+    'ar': {
+        'selflink-remove': u'روبوت: إزالة وصلات ذاتية',
+    },
+    # Author: Xuacu
+    'ast': {
+        'selflink-remove': u'Bot: Desaniciu d\'enllaces a la propia páxina',
+    },
+    'be-x-old': {
+        'selflink-remove': u'Робат: выдаленьне аўтаспасылкі',
+    },
+    # Author: Gwenn-Ael
+    'br': {
+        'selflink-remove': u'Robot : O tennañ amliammoù',
+    },
+    # Author: Edinwiki
+    'bs': {
+        'selflink-remove': u'Bot: Uklanjam linkovanje na sebe',
+    },
+    # Author: Fitoschido
+    'ca': {
+        'selflink-remove': u'Robot: eliminació d’autoenllaços',
+    },
+    # Author: Умар
+    'ce': {
+        'selflink-remove': u'Бот: дӀабаьккхина корта-хьажорг йозан юкъахь. хьажа кхин.',
+    },
+    'cs': {
+        'selflink-remove': u'Robot odstranil odkaz na název článku',
+    },
+    'da': {
+        'selflink-remove': u'Bot: fjerner selvreference',
+    },
+    'de': {
+        'selflink-remove': u'Bot: Entferne Selbstlinks',
+    },
+    # Author: Geraki
+    'el': {
+        'selflink-remove': u'Bot: Αφαίρεση αυτοσυνδέσμων',
+    },
+    # Author: Fitoschido
+    'es': {
+        'selflink-remove': u'Bot: eliminación de enlaces hacia la propia página',
+    },
+    # Author: Alirezaaa
+    'fa': {
+        'selflink-remove': u'ربات: حذف پیوند به خود',
+    },
+    'fr': {
+        'selflink-remove': u'Robot: Enlève autoliens',
+    },
+    # Author: Murma174
+    'frr': {
+        'selflink-remove': u'Bot: Nem rekursiif ferwisangen wech',
+    },
+    'he': {
+        'selflink-remove': u'בוט: מסיר קישורים של הדף לעצמו',
+    },
+    'hu': {
+        'selflink-remove': u'Bot: Önmagukra mutató hivatkozások eltávolítása',
+    },
+    # Author: Beta16
+    'it': {
+        'selflink-remove': u'Bot: rimuovo collegamento a se stesso',
+    },
+    # Author: Shirayuki
+    'ja': {
+        'selflink-remove': u'ロボットによる: 自己リンクの解除',
+    },
+    # Author: 아라
+    'ko': {
+        'selflink-remove': u'봇: 자기링크 제거',
+    },
+    'ksh': {
+        'selflink-remove': u'Bot: Ene Lengk vun de Sigg op sesch sellver, erus jenumme.',
+    },
+    # Author: Robby
+    'lb': {
+        'selflink-remove': u'Bot: Linken op déi selwecht Säit ewechhuelen',
+    },
+    # Author: Jagwar
+    'mg': {
+        'selflink-remove': u'Rôbô: Manova ny rohy manondro tena',
+    },
+    # Author: Bjankuloski06
+    'mk': {
+        'selflink-remove': u'Бот: Отстранување на самоодносни врски',
+    },
+    # Author: Praveenp
+    'ml': {
+        'selflink-remove': u'യന്ത്രം: തന്നിലേക്ക് തന്നെയുള്ള കണ്ണികൾ നീക്കുന്നു',
+    },
+    # Author: Anakmalaysia
+    'ms': {
+        'selflink-remove': u'Bot: Menggugurkan pautan sendiri',
+    },
+    'nl': {
+        'selflink-remove': u'Bot: verwijzingen naar pagina zelf verwijderd',
+    },
+    'nn': {
+        'selflink-remove': u'robot: fjerna sjølvlenkjer',
+    },
+    'no': {
+        'selflink-remove': u'robot: fjerner selvlenker',
+    },
+    'pl': {
+        'selflink-remove': u'Robot automatycznie usuwa linki zwrotne',
+    },
+    'pt': {
+        'selflink-remove': u'Bot: Retirando link para o próprio artigo',
+    },
+    # Author: !Silent
+    'pt-br': {
+        'selflink-remove': u'Bot: removendo ligações para o próprio artigo',
+    },
+    'ru': {
+        'selflink-remove': u'Бот: удалил заголовок-ссылку в тексте. см.',
+    },
+    # Author: Lokal Profil
+    'sv': {
+        'selflink-remove': u'Bot: Tar bort självlänkar',
+    },
+    # Author: Base
+    'uk': {
+        'selflink-remove': u'Бот: вилучення посилань на цю саму сторінку',
+    },
+    # Author: Minh Nguyen
+    'vi': {
+        'selflink-remove': u'Bot: Bỏ các liên kết không đi đâu',
+    },
+    # Author: Liuxinyu970226
+    'zh': {
+        'selflink-remove': u'机器人：移除自链接',
+    },
+    # Author: Liuxinyu970226
+    'zh-hant': {
+        'selflink-remove': u'機械人: 刪除自我連結',
+    },
+};
