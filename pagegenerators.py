bcad3e93c9c576ad2f9a13fff3095460dc96eb73
I am not me <impeccabletiming@gmail.com>
2014-12-02 20:38:02 -0500

    Modify pagegenerators.py

--- pagegenerators.py
+++ pagegenerators.py
@@ -1210,7 +1210,7 @@ def main(*args):
         gen = genFactory.getCombinedGenerator()
         if gen:
             for i, page in enumerate(gen, start=1):
-                pywikibot.output("%4d: %s" % (i, page.title()),
+                pywikibot.output("%s" % (page.title()),
                                  toStdout=True)
         else:
             pywikibot.showHelp()
